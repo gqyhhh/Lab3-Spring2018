4473b6173b959d4fea81381c2de55c29b303b7fd
qg3 <qg3@illinois.edu>
2018-02-07 18:57:58 -0600

    initial program

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String unused) {
-        System.out.println("Ran");
+    public static void main(String[] unused) {
+        String result = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(result.length());
+
+    }
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
